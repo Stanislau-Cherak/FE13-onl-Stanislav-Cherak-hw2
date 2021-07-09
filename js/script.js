ee0f3dadbe2f3428d6be23f5eb4a068ad3bbeb13
Stanislav-Cherak <sirStanislav27@gmail.com>
2021-07-10 01:42:34 +0300

    landing ready without adaptive

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
-function ibg() {
-	let ibg = document.querySelectorAll(".ibg");
-	for (var i = 0; i < ibg.length; i++) {
-		if (ibg[i].querySelector('img')) {
-			ibg[i].style.backgroundImage = 'url(' + ibg[i].querySelector('img').getAttribute('src') + ')';
-		}
-	}
-
-}
-
-ibg();
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows:false,
+        dots: true,
+        fade: true,
+        speed: 2000,
+        easing:'ease',
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        waitForAnimate: false,
+        appendDots:$('.slider_description'),
+    });
+});
